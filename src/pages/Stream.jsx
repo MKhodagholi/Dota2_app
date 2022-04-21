622efbe97c102a15b6bce5df84d189fd72900430
MKhodagholi <MKhodaagholi@gmail.com>
2022-04-21 12:25:18 +0430

    9. Coding Stream Component

--- src/pages/Stream.jsx
+++ src/pages/Stream.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import CardHero from "../components/CardHero";
 
 import Search from "../components/Search";
 
 const Stream = () => {
   return (
-    <div>
+    <div className="stream">
       <Search />
+      <div className="stream-content">
+        <div className="title">
+          Learn You <br />
+          Favorite Hero
+        </div>
+        <CardHero />
+      </div>
     </div>
   );
 };
