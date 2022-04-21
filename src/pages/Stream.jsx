1a6b223cf1b80e97e81c384fc79ed8e647216b48
MKhodagholi <MKhodaagholi@gmail.com>
2022-04-21 15:50:14 +0430

    12. add cardHeros in Stream Component

--- src/pages/Stream.jsx
+++ src/pages/Stream.jsx
@@ -3,6 +3,8 @@ import CardHero from "../components/CardHero";
 
 import Search from "../components/Search";
 
+import { cardHeroData } from "../data";
+
 const Stream = () => {
   return (
     <div className="stream">
@@ -12,7 +14,16 @@ const Stream = () => {
           Learn You <br />
           Favorite Hero
         </div>
-        <CardHero />
+        <div className="card-heros">
+          {cardHeroData.map((card, index) => (
+            <CardHero
+              name={card.name}
+              views={card.views}
+              img={card.img}
+              key={index}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
