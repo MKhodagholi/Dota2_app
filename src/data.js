fdd14923d9119e615e09f446de15729d942fd825
MKhodagholi <MKhodaagholi@gmail.com>
2022-04-21 15:47:08 +0430

    10. add img and data for CardHero

---
+++ src/data.js
@@ -0,0 +1,26 @@
+import Axe from "./img/axe.png";
+import Ert from "./img/earthshaker.png";
+import Blood from "./img/blood.png";
+
+export const cardHeroData = [
+  {
+    name: "Axe",
+    views: "37",
+    img: Axe,
+  },
+  {
+    name: "Shaker",
+    views: "34",
+    img: Ert,
+  },
+  {
+    name: "Seeker",
+    views: "30",
+    img: Blood,
+  },
+  {
+    name: "Mars",
+    views: "24",
+    img: Axe,
+  },
+];
