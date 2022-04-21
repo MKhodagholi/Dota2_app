df2ed4f2b274b163154db00cff6ed07315e2168d
MKhodagholi <MKhodaagholi@gmail.com>
2022-04-21 09:42:34 +0430

    3. initial Stream Page

---
+++ src/pages/Stream.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Search from "../components/Search";
+
+const Stream = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default Stream;
